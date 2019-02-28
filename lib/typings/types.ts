40b4656bdc1a5bb57bde7eba835a60695dc6ab83
ipcrm <mcadorette@gmail.com>
2019-02-28 06:56:31 -0500

    Fix empty channels issue
    Add a couple of checks to see if the channels array is empty and do not
    send message if that is the case (which it will be for unlinked repos).

--- lib/typings/types.ts
+++ lib/typings/types.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+ /* tslint:disable */
+
 // ====================================================
 // START: Typescript template
 // ====================================================
